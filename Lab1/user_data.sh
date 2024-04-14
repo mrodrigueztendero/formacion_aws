4afbc231b5cb215ebdcf176060b458dbffa12a75
Martín Rodríguez Tendero <mrodriguez.tendero@gmail.com>
2024-04-14 23:54:42 +0200

    Initial commit

--- Lab1/user_data.sh
+++ Lab1/user_data.sh
@@ -3,4 +3,4 @@ yum update –y
 yum install nginx -y
 systemctl start nginx
 systemctl enable nginx
-echo "<h3> Welcome to my NGINX web server! User data instillation was a SUCCESS! </h3>" > /usr/share/nginx/html/index.html
+echo "<h3> Welcome to my NGINX web server!</h3> <h2>User data instillation was a SUCCESS! </h2>" > /usr/share/nginx/html/index.html
